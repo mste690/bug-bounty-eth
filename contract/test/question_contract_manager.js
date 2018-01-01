72eb85c04810b3eafd3bfb83a1156cab35351a46
mste690 <mste690@gmail.com>
2018-01-01 17:53:36 +1300

    Add tests for verifying created Question contract and addresses

--- contract/test/question_contract_manager.js
+++ contract/test/question_contract_manager.js
@@ -189,6 +189,68 @@ contract('QuestionContractManager', function(accounts) {
     var questionContract = Question.at(result);
     //check that the author is the message sender
     assert.equal(questionContract.author, accounts[0]);
-
   });
+
+  it("should have same Question contract balance as max bounty", async function() {
+    var question_contract_manager = await QuestionContractManager.deployed();
+    var result = await question_contract_manager.SubmitQuestion("questiontext", ["tag1", "tag2"], 2017, 500, 1000, 28, 20, {value: 1020, from: accounts[0]});
+    //check that the result contract balance is bounty maxValue
+    assert.equal(web3.eth.getBalance(result), 1000);
+  });
+
+  it("should have Question isClosed as false", async function() {
+    var question_contract_manager = await QuestionContractManager.deployed();
+    var result = await question_contract_manager.SubmitQuestion("questiontext", ["tag1", "tag2"], 2017, 500, 1000, 28, 20, {value: 1020, from: accounts[0]});
+    var questionContract = Question.at(result);
+    //check that the author is the message sender
+    assert.isFalse(questionContract.isClosed, "Question isClosed should be false on submission");
+  });
+
+  it("should have Question address in managers questions array", async function() {
+    var question_contract_manager = await QuestionContractManager.deployed();
+    var result = await question_contract_manager.SubmitQuestion("questiontext", ["tag1", "tag2"], 2017, 500, 1000, 28, 20, {value: 1020, from: accounts[0]});
+    //check that result address is in questions
+    assert.isOk(question_contract_manager.questions.includes(result), 'questions array should contain new question address');
+  });
+
+  it("should have no questions for new contract manager", async function() {
+    let new_question_contract_manager = await QuestionContractManager.new();
+    assert.isEmpty(new_question_contract_manager.questions, 'New manager should have no questions');
+  });
+
+  it("should have single address in new manager after submit", async function() {
+    let new_question_contract_manager = await QuestionContractManager.new();
+    let result = await new_question_contract_manager.SubmitQuestion("questiontext", ["tag1", "tag2"], 2017, 500, 1000, 28, 20, {value: 1020, from: accounts[0]});
+    //check that result address is in questions
+    assert.isOk(new_question_contract_manager.questions.includes(result), 'questions array should contain new question address');
+    assert.lengthOf(new_question_contract_manager.questions, 1, 'questions should contain 1 address');
+  });
+
+  it("should have two addresses in new manager after two submissions", async function() {
+    let new_question_contract_manager = await QuestionContractManager.new();
+    let result = await new_question_contract_manager.SubmitQuestion("questiontext", ["tag1", "tag2"], 2017, 500, 1000, 28, 20, {value: 1020, from: accounts[0]});
+    //check that result address is in questions
+    assert.isOk(new_question_contract_manager.questions.includes(result), 'questions array should contain new question address');
+    assert.lengthOf(new_question_contract_manager.questions, 1, 'questions should contain 1 address');
+
+    let result2 = await new_question_contract_manager.SubmitQuestion("questiontext", ["tag1", "tag2"], 2017, 500, 1000, 28, 20, {value: 1020, from: accounts[0]});
+    //check that second result address is in questions
+    assert.isOk(new_question_contract_manager.questions.includes(result2), 'questions array should contain new question address');
+    assert.lengthOf(new_question_contract_manager.questions, 2, 'questions should contain 2 addresses');
+  });
+
+  it("should have correct question data at stored address", async function() {
+    let new_question_contract_manager = await QuestionContractManager.new();
+    let result = await new_question_contract_manager.SubmitQuestion("questiontext", ["tag1", "tag2"], 2017, 500, 1000, 28, 20, {value: 1020, from: accounts[0]});
+    //check that stored address is in questions
+    let questionContract = Question.at(new_question_contract_manager.questions[0]);
+    //check that the instantiated contract has a questiontext field that matches the input
+    assert.equal(questionContract.questiontext, "questiontext");
+    assert.equal(questionContract.tags, ["tag1", "tag2"]);
+    assert.equal(questionContract.submittedTime, 2017);
+    assert.equal(questionContract.bounty.minValue, 500);
+    assert.equal(questionContract.bounty.maxValue, 1000);
+    assert.equal(questionContract.bounty.timeToMaxValue, 28);
+  });
+
 });
