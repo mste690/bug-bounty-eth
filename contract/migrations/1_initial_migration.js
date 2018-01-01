1a0487af7797c1a276edc1392d61d5cc49fb0db1
bumblepie <luke@bumblepie.space>
2018-01-01 18:50:18 +1300

    Remove Question contract deploy from migrations

--- contract/migrations/1_initial_migration.js
+++ contract/migrations/1_initial_migration.js
@@ -1,9 +1,7 @@
 var Migrations = artifacts.require("./Migrations.sol");
 var QuestionContractManager = artifacts.require("./QuestionContractManager.sol");
-var Question = artifacts.require("./Question.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(Migrations);
   deployer.deploy(QuestionContractManager);
-  deployer.deploy(Question);
 };
