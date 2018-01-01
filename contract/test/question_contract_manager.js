8809dc50060888f67417f2a971584aa9c1de36f4
bumblepie <luke@bumblepie.space>
2018-01-01 18:23:47 +1300

    Refactor failing transactions duplicated code into a function

--- contract/test/question_contract_manager.js
+++ contract/test/question_contract_manager.js
@@ -8,17 +8,21 @@ contract('QuestionContractManager', function(accounts) {
     question_contract_manager = await QuestionContractManager.deployed();
   });
 
+  expectTransactionToFail = async (transaction) => {
+    try {
+      await transaction;
+    } catch (e) {
+      return true;
+    }
+    assert.fail("Tx did not fail", "Tx should fail", "Expected transaction to fail, but it did not");
+  }
+
   it("should not fail with valid input", async function() {
     var result = await question_contract_manager.SubmitQuestion("questiontext", ["tag1", "tag2"], 2017, 500, 1000, 28, 20, {value: 1020});
   });
 
   it("should fail if questionText is an empty string", async function() {
-    try {
-      await question_contract_manager.SubmitQuestion("", ["tag1", "tag2"], 2017, 500, 1000, 28, 20, {value: 1020});
-    } catch (e) {
-      return true;
-    }
-    assert.fail("Tx did not fail", "Tx should fail", "Expected transaction to fail, but it did not");
+    await expectTransactionToFail(question_contract_manager.SubmitQuestion("", ["tag1", "tag2"], 2017, 500, 1000, 28, 20, {value: 1020}))
   });
 
   it("should fail if questionText is longer than the maximum character length", async function() {
@@ -27,22 +31,11 @@ contract('QuestionContractManager', function(accounts) {
     for (let i = 0; i <= MAX_CHARS; i++) {
       longString += "a";
     }
-
-    try {
-      await question_contract_manager.SubmitQuestion(longString, ["tag1", "tag2"], 2017, 500, 1000, 28, 20, {value: 1020});
-    } catch (e) {
-      return true;
-    }
-    assert.fail("Tx did not fail", "Tx should fail", "Expected transaction to fail, but it did not");
+    await expectTransactionToFail(question_contract_manager.SubmitQuestion(longString, ["tag1", "tag2"], 2017, 500, 1000, 28, 20, {value: 1020}));
   });
 
   it("should fail if the list of tags contains an empty string", async function() {
-    try {
-      await question_contract_manager.SubmitQuestion("questiontext", ["tag1", ""], 2017, 500, 1000, 28, 20, {value: 1020});
-    } catch (e) {
-      return true;
-    }
-    assert.fail("Tx did not fail", "Tx should fail", "Expected transaction to fail, but it did not");
+    await expectTransactionToFail(question_contract_manager.SubmitQuestion("questiontext", ["tag1", ""], 2017, 500, 1000, 28, 20, {value: 1020}));
   });
 
   it("should fail if the list of tags contains more than the maximum number of allowed tags", async function() {
@@ -52,111 +45,52 @@ contract('QuestionContractManager', function(accounts) {
       tags.push(`tag ${i}`);
     }
 
-    try {
-      await question_contract_manager.SubmitQuestion("questiontext", tags, 2017, 500, 1000, 28, 20, {value: 1020});
-    } catch (e) {
-      return true;
-    }
-    assert.fail("Tx did not fail", "Tx should fail", "Expected transaction to fail, but it did not");
+    await expectTransactionToFail(question_contract_manager.SubmitQuestion("questiontext", tags, 2017, 500, 1000, 28, 20, {value: 1020}));
   });
 
   it("should fail if the list of tags contains duplicate tags", async function() {
-    try {
-      await question_contract_manager.SubmitQuestion("questiontext", ["tag1", "tag1"], 2017, 500, 1000, 28, 20, {value: 1020});
-    } catch (e) {
-      return true;
-    }
-    assert.fail("Tx did not fail", "Tx should fail", "Expected transaction to fail, but it did not");
+    await expectTransactionToFail(question_contract_manager.SubmitQuestion("questiontext", ["tag1", "tag1"], 2017, 500, 1000, 28, 20, {value: 1020}));
   });
 
   it("should fail if the bountyMinValue is 0", async function() {
-    try {
-      await question_contract_manager.SubmitQuestion("questiontext", ["tag1", "tag2"], 2017, 0, 1000, 28, 20, {value: 1020});
-    } catch (e) {
-      return true;
-    }
-    assert.fail("Tx did not fail", "Tx should fail", "Expected transaction to fail, but it did not");
+    await expectTransactionToFail(question_contract_manager.SubmitQuestion("questiontext", ["tag1", "tag2"], 2017, 0, 1000, 28, 20, {value: 1020}));
   });
 
   it("should fail if the bountyMinValue is negative", async function() {
-    try {
-      await question_contract_manager.SubmitQuestion("questiontext", ["tag1", "tag2"], 2017, -1, 1000, 28, 20, {value: 1020});
-    } catch (e) {
-      return true;
-    }
-    assert.fail("Tx did not fail", "Tx should fail", "Expected transaction to fail, but it did not");
+    await expectTransactionToFail(question_contract_manager.SubmitQuestion("questiontext", ["tag1", "tag2"], 2017, -1, 1000, 28, 20, {value: 1020}));
   });
 
   it("should fail if the bountyMinValue is greater than bountyMaxValue", async function() {
-    try {
-      await question_contract_manager.SubmitQuestion("questiontext", ["tag1", "tag2"], 2017, 1001, 1000, 28, 20, {value: 1020});
-    } catch (e) {
-      return true;
-    }
-    assert.fail("Tx did not fail", "Tx should fail", "Expected transaction to fail, but it did not");
+    await expectTransactionToFail(question_contract_manager.SubmitQuestion("questiontext", ["tag1", "tag2"], 2017, 1001, 1000, 28, 20, {value: 1020}));
   });
 
   it("should fail if the bountyMinValue is equal to bountyMaxValue", async function() {
-    try {
-      await question_contract_manager.SubmitQuestion("questiontext", ["tag1", "tag2"], 2017, 1000, 1000, 28, 20, {value: 1020});
-    } catch (e) {
-      return true;
-    }
-    assert.fail("Tx did not fail", "Tx should fail", "Expected transaction to fail, but it did not");
+    await expectTransactionToFail(question_contract_manager.SubmitQuestion("questiontext", ["tag1", "tag2"], 2017, 1000, 1000, 28, 20, {value: 1020}));
   });
 
   it("should fail if the value given to the contract is less than to the bountyMaxValue plus the tip", async function() {
-    try {
-      await question_contract_manager.SubmitQuestion("questiontext", ["tag1", "tag2"], 2017, 500, 1000, 28, 20, {value: 1000});
-    } catch (e) {
-      return true;
-    }
-    assert.fail("Tx did not fail", "Tx should fail", "Expected transaction to fail, but it did not");
+    await expectTransactionToFail(question_contract_manager.SubmitQuestion("questiontext", ["tag1", "tag2"], 2017, 500, 1000, 28, 20, {value: 1000}));
   });
 
   it("should fail if the value given to the contract is greater than to the bountyMaxValue plus the tip", async function() {
-    
-    try {
-      await question_contract_manager.SubmitQuestion("questiontext", ["tag1", "tag2"], 2017, 500, 1000, 28, 20, {value: 1030});
-    } catch (e) {
-      return true;
-    }
-    assert.fail("Tx did not fail", "Tx should fail", "Expected transaction to fail, but it did not");
+    await expectTransactionToFail(question_contract_manager.SubmitQuestion("questiontext", ["tag1", "tag2"], 2017, 500, 1000, 28, 20, {value: 1030}));
   });
 
   it("should fail if the bounty timeToMaxValue is negative", async function() {
-    
-    try {
-      await question_contract_manager.SubmitQuestion("questiontext", ["tag1", "tag2"], 2017, 500, 1000, -5, 20, {value: 1020});
-    } catch (e) {
-      return true;
-    }
-    assert.fail("Tx did not fail", "Tx should fail", "Expected transaction to fail, but it did not");
+    await expectTransactionToFail(question_contract_manager.SubmitQuestion("questiontext", ["tag1", "tag2"], 2017, 500, 1000, -5, 20, {value: 1020}));
   });
 
   it("should fail if the bounty timeToMaxValue is greater than 28 days", async function() {
     
     const LONG_TIME = (28 * 24 * 60 * 60) + 1
-    try {
-      await question_contract_manager.SubmitQuestion("questiontext", ["tag1", "tag2"], 2017, 500, 1000, LONG_TIME, 20, {value: 1020});
-    } catch (e) {
-      return true;
-    }
-    assert.fail("Tx did not fail", "Tx should fail", "Expected transaction to fail, but it did not");
+    await expectTransactionToFail(question_contract_manager.SubmitQuestion("questiontext", ["tag1", "tag2"], 2017, 500, 1000, LONG_TIME, 20, {value: 1020}));
   });
 
   it("should fail if the bounty tip is less than 1% of the bounty max value", async function() {
-    
-    try {
-      await question_contract_manager.SubmitQuestion("questiontext", ["tag1", "tag2"], 2017, 500, 1000, 28, 19, {value: 1019});
-    } catch (e) {
-      return true;
-    }
-    assert.fail("Tx did not fail", "Tx should fail", "Expected transaction to fail, but it did not");
+    await expectTransactionToFail(question_contract_manager.SubmitQuestion("questiontext", ["tag1", "tag2"], 2017, 500, 1000, 28, 19, {value: 1019}));
   });
 
   it("should return address to question contract", async function() {
-    var question_contract_manager = await QuestionContractManager.deployed();
     var result = await question_contract_manager.SubmitQuestion("questiontext", ["tag1", "tag2"], 2017, 500, 1000, 28, 20, {value: 1020});
     //check the result is a valid ethereum address
     assert.isOk(web3.isAddress(result), 'SubmitQuestion did not return valid ethereum address');
