e71178ba97e3f0f18477d6fc18c6a26afc0da9fe
bumblepie <luke@bumblepie.space>
2018-01-02 16:09:36 +1300

    Add require statements to limit valid input of QuestionContractManager SubmitQuestion function

--- contract/contracts/QuestionContractManager.sol
+++ contract/contracts/QuestionContractManager.sol
@@ -1,15 +1,41 @@
 pragma solidity ^0.4.4;
 
 contract QuestionContractManager {
-  
+
+  uint constant MAX_QUESTION_TEXT_LENGTH = 2048;
+  uint constant MAX_TAGS = 10;
+  uint constant MAX_TAG_TEXT_LENGTH = 32;
+
   address[] questions;
   
   function QuestionContractManager() {
     //constructor
   }
   
-  function SubmitQuestion(string questionText, bytes32[] tags, uint submittedTime, uint bountyMinValue, uint bountyMaxValue, uint 
-bountyTimeToMaxValue, uint tip) public payable returns(address) {
-	//require(1 == 0);  
+  function SubmitQuestion(string questionText, bytes32[] tags, uint submittedTime, uint bountyMinValue, uint bountyMaxValue, uint bountyTimeToMaxValue, uint tip) public payable returns(address) { 
+    require(bytes(questionText).length > 0);
+    require(bytes(questionText).length <= MAX_QUESTION_TEXT_LENGTH);
+    require(tags.length < MAX_TAGS);
+
+    bytes32[] memory seenTags = new bytes32[](10);
+    for (uint i = 0; i < tags.length; i++) {
+      uint tagLength = tags[i].length;
+      require(tagLength > 0);
+      require(tagLength <= MAX_TAG_TEXT_LENGTH);
+
+      //check for duplicates
+      for(uint j = 0; j < seenTags.length; j++) {
+        require(tags[i] != seenTags[j]);
+      }
+
+      seenTags[i] = tags[i];
+    }
+
+    require(bountyMinValue > 0);
+    require(bountyMinValue <= bountyMaxValue);
+    require(msg.value == bountyMaxValue + tip);
+    require(bountyTimeToMaxValue >= 0);
+    require(bountyTimeToMaxValue <= 28 days);
+    require(tip >= bountyMaxValue/100);
   }
 }
