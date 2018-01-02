db87450bd7c310df37885fc4448133f2bc780c53
mste690 <mste690@gmail.com>
2018-01-02 15:45:56 +1300

    Write constructor for Question contract

--- contract/contracts/Question.sol
+++ contract/contracts/Question.sol
@@ -1,22 +1,37 @@
 pragma solidity ^0.4.4;
 
 contract Question {
-  
+
   uint author;
   string questionText;
-  string[] tags;
+  bytes32[] tags;
   Bounty bounty;
   uint submittedTime;
   bool isClosed;
-  
+
   struct Bounty {
     uint minValue;
     uint maxValue;
     uint timeToMaxValue;
   }
 
-  function Question() {
-    // constructor
+  function Question(
+      uint _author,
+      string _questionText,
+      bytes32[] _tags,
+      uint _submittedTime,
+      uint _bountyMinValue,
+      uint _bountyMaxValue,
+      uint _bountyTimeToMaxValue
+  )
+      public
+  {
+    author = _author;
+    questionText = _questionText;
+    tags = _tags;
+    bounty = Bounty(_bountyMinValue, _bountyMaxValue, _bountyTimeToMaxValue);
+    submittedTime = _submittedTime;
+    isClosed = false;
   }
 
 }
