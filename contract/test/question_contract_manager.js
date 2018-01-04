f5e4dc8f7f4c0784473b09bb66dc9aa473974e37
bumblepie <luke@bumblepie.space>
2018-01-04 23:16:47 +1300

    Add spacing between arrow and open curly brackets for readability

--- contract/test/question_contract_manager.js
+++ contract/test/question_contract_manager.js
@@ -64,13 +64,13 @@ contract('QuestionContractManager', (accounts) => {
       await submitQuestionPromise(question_contract_manager, getDefaultValues());
     });
 
-    it('should fail if questionText is an empty string', async () =>{
+    it('should fail if questionText is an empty string', async () => {
       let values = getDefaultValues();
       values.questionText = '';
       await expectTransactionToFail(submitQuestionPromise(question_contract_manager, values));
     });
 
-    it('should fail if questionText is longer than the maximum character length', async () =>{
+    it('should fail if questionText is longer than the maximum character length', async () => {
       const MAX_CHARS = 2048;
       let longString = '';
       for (let i = 0; i <= MAX_CHARS; i++) {
@@ -82,13 +82,13 @@ contract('QuestionContractManager', (accounts) => {
       await expectTransactionToFail(submitQuestionPromise(question_contract_manager, values));
     });
 
-    it('should fail if the list of tags contains an empty string', async () =>{
+    it('should fail if the list of tags contains an empty string', async () => {
       let values = getDefaultValues();
       values.tags[0] = '';
       await expectTransactionToFail(submitQuestionPromise(question_contract_manager, values));
     });
 
-    it('should not fail if the list of tags contains exactly the maximum number of allowed tags', async () =>{
+    it('should not fail if the list of tags contains exactly the maximum number of allowed tags', async () => {
       const MAX_TAGS = 10;
       let tags = [];
       for (let i = 0; i < MAX_TAGS; i++) {
@@ -100,7 +100,7 @@ contract('QuestionContractManager', (accounts) => {
       await submitQuestionPromise(question_contract_manager, values);
     });
 
-    it('should fail if the list of tags contains more than the maximum number of allowed tags', async () =>{
+    it('should fail if the list of tags contains more than the maximum number of allowed tags', async () => {
       const MAX_TAGS = 10;
       let tags = [];
       for (let i = 0; i <= MAX_TAGS; i++) {
@@ -112,31 +112,31 @@ contract('QuestionContractManager', (accounts) => {
       await expectTransactionToFail(submitQuestionPromise(question_contract_manager, values));
     });
 
-    it('should fail if the list of tags contains duplicate tags', async () =>{
+    it('should fail if the list of tags contains duplicate tags', async () => {
       let values = getDefaultValues();
       values.tags = ['tag1', 'tag1'];
       await expectTransactionToFail(submitQuestionPromise(question_contract_manager, values));
     });
 
-    it('should fail if the bountyMinValue is 0', async () =>{
+    it('should fail if the bountyMinValue is 0', async () => {
       let values = getDefaultValues();
       values.bountyMinValue = 0;
       await expectTransactionToFail(submitQuestionPromise(question_contract_manager, values));
     });
 
-    it('should fail if the bountyMinValue is negative', async () =>{
+    it('should fail if the bountyMinValue is negative', async () => {
       let values = getDefaultValues();
       values.bountyMinValue = -1;
       await expectTransactionToFail(submitQuestionPromise(question_contract_manager, values));
     });
 
-    it('should fail if the bountyMinValue is greater than bountyMaxValue', async () =>{
+    it('should fail if the bountyMinValue is greater than bountyMaxValue', async () => {
       let values = getDefaultValues();
       values.bountyMinValue = values.bountyMaxValue + 1;
       await expectTransactionToFail(submitQuestionPromise(question_contract_manager, values));
     });
 
-    it('should fail if the value given to the contract is less than to the bountyMaxValue plus the tip', async () =>{
+    it('should fail if the value given to the contract is less than to the bountyMaxValue plus the tip', async () => {
       let values = getDefaultValues();
       values.transactionObject = {
         value: values.bountyMaxValue + values.tip - 1
@@ -144,7 +144,7 @@ contract('QuestionContractManager', (accounts) => {
       await expectTransactionToFail(submitQuestionPromise(question_contract_manager, values));
     });
 
-    it('should fail if the value given to the contract is greater than to the bountyMaxValue plus the tip', async () =>{
+    it('should fail if the value given to the contract is greater than to the bountyMaxValue plus the tip', async () => {
       let values = getDefaultValues();
       values.transactionObject = {
         value: values.bountyMaxValue + values.tip + 1
@@ -152,26 +152,26 @@ contract('QuestionContractManager', (accounts) => {
       await expectTransactionToFail(submitQuestionPromise(question_contract_manager, values));
     });
 
-    it('should fail if the bounty timeToMaxValue is negative', async () =>{
+    it('should fail if the bounty timeToMaxValue is negative', async () => {
       let values = getDefaultValues();
       values.bountyTimeToMaxValue = -1;
       await expectTransactionToFail(submitQuestionPromise(question_contract_manager, values));
     });
 
-    it('should fail if the bounty timeToMaxValue is greater than bountyTimeToMaxValue days', async () =>{
+    it('should fail if the bounty timeToMaxValue is greater than bountyTimeToMaxValue days', async () => {
       const TWENTY_EIGHT_DAYS = 28 * 24 * 60 * 60;
       let values = getDefaultValues();
       values.bountyTimeToMaxValue = TWENTY_EIGHT_DAYS + 1;
       await expectTransactionToFail(submitQuestionPromise(question_contract_manager, values));
     });
 
-    it('should fail if the bounty tip is less than 1% of the bounty max value', async () =>{
+    it('should fail if the bounty tip is less than 1% of the bounty max value', async () => {
       let values = getDefaultValues();
       values.tip = (values.bountyMaxValue / 100) -1;
       await expectTransactionToFail(submitQuestionPromise(question_contract_manager, values));
     });
 
-    it('should return address to question contract', async () =>{
+    it('should return address to question contract', async () => {
       await submitQuestionPromise(question_contract_manager, getDefaultValues());
       const result = await getCreatedQuestionContractAddress(question_contract_manager);
       //check the result is a valid ethereum address
@@ -181,7 +181,7 @@ contract('QuestionContractManager', (accounts) => {
       assert.equal(await questionContract.questionText.call(), getDefaultValues().questionText);
     });
 
-    it('should create Question contract with correct inputs', async () =>{
+    it('should create Question contract with correct inputs', async () => {
       await submitQuestionPromise(question_contract_manager, getDefaultValues());
       const result = await getCreatedQuestionContractAddress(question_contract_manager);
       const questionContract = Question.at(result);
@@ -196,7 +196,7 @@ contract('QuestionContractManager', (accounts) => {
       assert.equal(bounty[2], getDefaultValues().bountyTimeToMaxValue);
     });
 
-    it('should have Question author as message sender', async () =>{
+    it('should have Question author as message sender', async () => {
       let values = getDefaultValues();
       values.transactionObject.from = accounts[0]
       await submitQuestionPromise(question_contract_manager, values);
@@ -206,14 +206,14 @@ contract('QuestionContractManager', (accounts) => {
       assert.equal(await questionContract.author.call(), accounts[0]);
     });
 
-    it('should have same Question contract balance as max bounty', async () =>{
+    it('should have same Question contract balance as max bounty', async () => {
       await submitQuestionPromise(question_contract_manager, getDefaultValues());
       const result = await getCreatedQuestionContractAddress(question_contract_manager);
       //check that the result contract balance is bounty maxValue
       assert.equal(web3.eth.getBalance(result), getDefaultValues().bountyMaxValue);
     });
 
-    it('should increase the owners account balance by the tip amount', async () =>{
+    it('should increase the owners account balance by the tip amount', async () => {
       const new_question_contract_manager = await QuestionContractManager.new({from: accounts[0]});
 
       const initialBalance = web3.eth.getBalance(accounts[0]);
@@ -224,7 +224,7 @@ contract('QuestionContractManager', (accounts) => {
       assert.deepEqual(web3.eth.getBalance(accounts[0]), initialBalance.plus(getDefaultValues().tip));
     });
 
-    it('should have Question isClosed as false', async () =>{
+    it('should have Question isClosed as false', async () => {
       await submitQuestionPromise(question_contract_manager, getDefaultValues());
       const result = await getCreatedQuestionContractAddress(question_contract_manager);
       const questionContract = Question.at(result);
@@ -232,7 +232,7 @@ contract('QuestionContractManager', (accounts) => {
       assert.isFalse(await questionContract.isClosed.call(), 'Question isClosed should be false on submission');
     });
 
-    it('should have Question address in managers questions array', async () =>{
+    it('should have Question address in managers questions array', async () => {
       await submitQuestionPromise(question_contract_manager, getDefaultValues());
       const result = await getCreatedQuestionContractAddress(question_contract_manager);
       //check that result address is in questions
@@ -240,13 +240,13 @@ contract('QuestionContractManager', (accounts) => {
       assert.isOk(questions.includes(result), 'questions array should contain new question address');
     });
 
-    it('should have no questions for new contract manager', async () =>{
+    it('should have no questions for new contract manager', async () => {
       const new_question_contract_manager = await QuestionContractManager.new();
       const questions = await new_question_contract_manager.getQuestions.call();
       assert.equal(questions.length, 0, 'New manager should have no questions');
     });
 
-    it('should have single address in new manager after submit', async () =>{
+    it('should have single address in new manager after submit', async () => {
       const new_question_contract_manager = await QuestionContractManager.new();
       await submitQuestionPromise(new_question_contract_manager, getDefaultValues());
       const result = await getCreatedQuestionContractAddress(new_question_contract_manager);
@@ -257,7 +257,7 @@ contract('QuestionContractManager', (accounts) => {
       assert.lengthOf(questions, 1, 'questions should contain 1 address');
     });
 
-    it('should have two addresses in new manager after two submissions', async () =>{
+    it('should have two addresses in new manager after two submissions', async () => {
       const new_question_contract_manager = await QuestionContractManager.new();
       await submitQuestionPromise(new_question_contract_manager, getDefaultValues());
       const result = await getCreatedQuestionContractAddress(new_question_contract_manager);
@@ -276,7 +276,7 @@ contract('QuestionContractManager', (accounts) => {
       assert.lengthOf(questions, 2, 'questions should contain 2 addresses');
     });
 
-    it('should have correct question data at stored address', async () =>{
+    it('should have correct question data at stored address', async () => {
       const new_question_contract_manager = await QuestionContractManager.new();
       await submitQuestionPromise(new_question_contract_manager, getDefaultValues());      
       //check that stored address is in questions
